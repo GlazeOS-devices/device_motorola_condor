8987a2c2fab6b5e2861c8f2b7c5a465929241208
NoelMacwan <yulespencer@gmail.com>
2017-03-20 18:07:58 +0530

    condor: rebrand to GlazeOS

--- glaze.mk
+++ glaze.mk
@@ -1,8 +1,8 @@
 $(call inherit-product, device/motorola/condor/full_condor.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Glaze stuff.
+$(call inherit-product, vendor/glaze/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_condor
+PRODUCT_NAME := glaze_condor
 PRODUCT_RELEASE_NAME := MOTO E
